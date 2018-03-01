08cf3da24bb3217551b7de2eba8f22a53e8fdae0
Janita <janita_irfan7@yahoo.com>
2018-03-01 00:46:07 +0000

    code for prime number generator

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primenumbers
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            int i, j;
+
+            for (i = 2; i < 1000; i++)
+            {
+                for (j = 2; j <= (i/j); j++)
+                {
+                    //when prime
+                    if ((i%j) == 0) break; 
+                }
+                if (j > (i/j))
+                {
+                    Console.WriteLine("Prime Number : {0}", i);
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
